52f9261233baa5f4a759c7679ddfbd4bcb0e3d82
carricom <matthieu.carrico@etu.u-pec.fr>
2020-04-09 20:00:24 +0200

    ok

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Creer"},{"p":"<Unnamed>","l":"Grille"},{"p":"<Unnamed>","l":"Grilleauto"},{"p":"<Unnamed>","l":"Solveur"},{"p":"<Unnamed>","l":"Tableau"}]
